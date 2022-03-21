a201dfb02f132acabafa21bd234c75d8c365cbea
Ampareeshan <ambarishbalaji1908@gmail.com>
2022-03-21 18:18:32 +0530

    Added SNI Proxy
    Co-Authored-By: Vishvabala <94888653+vishisfish@users.noreply.github.com>
    Co-Authored-By: keerthana2111 <75537043+keerthana2111@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/tls"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strings"
 	"sync"
+	"time"
 
 	proxyproto "github.com/pires/go-proxyproto"
 )
@@ -20,6 +25,20 @@ func main() {
 		fmt.Println("error in starting the tcp server")
 	} else {
 		fmt.Println("server started on port 5000")
+	}
+
+	//setting up tls server
+	l, err := net.Listen("tcp", ":443")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleTlsConnection(conn)
 	}
 
 	// setting up udp server
@@ -39,7 +58,7 @@ func main() {
 	go handleUDP(udp)
 	go handleTCP(tcp)
 	go handlePP(pp)
-	
+
 	wg.Wait()
 }
 
@@ -90,4 +109,96 @@ func process(conn net.Conn) {
 	fmt.Fprint(conn, conn.RemoteAddr())
 	//print client ip
 	fmt.Print(conn.RemoteAddr())
+}
+
+//peek function for tls
+
+func peekClientHello(reader io.Reader) (*tls.ClientHelloInfo, io.Reader, error) {
+	peekedBytes := new(bytes.Buffer)
+	hello, err := readClientHello(io.TeeReader(reader, peekedBytes))
+	if err != nil {
+		return nil, nil, err
+	}
+	return hello, io.MultiReader(peekedBytes, reader), nil
+}
+
+type readOnlyConn struct {
+	reader io.Reader
+}
+
+func (conn readOnlyConn) Read(p []byte) (int, error)         { return conn.reader.Read(p) }
+func (conn readOnlyConn) Write(p []byte) (int, error)        { return 0, io.ErrClosedPipe }
+func (conn readOnlyConn) Close() error                       { return nil }
+func (conn readOnlyConn) LocalAddr() net.Addr                { return nil }
+func (conn readOnlyConn) RemoteAddr() net.Addr               { return nil }
+func (conn readOnlyConn) SetDeadline(t time.Time) error      { return nil }
+func (conn readOnlyConn) SetReadDeadline(t time.Time) error  { return nil }
+func (conn readOnlyConn) SetWriteDeadline(t time.Time) error { return nil }
+
+func readClientHello(reader io.Reader) (*tls.ClientHelloInfo, error) {
+	var hello *tls.ClientHelloInfo
+
+	err := tls.Server(readOnlyConn{reader: reader}, &tls.Config{
+		GetConfigForClient: func(argHello *tls.ClientHelloInfo) (*tls.Config, error) {
+			hello = new(tls.ClientHelloInfo)
+			*hello = *argHello
+			return nil, nil
+		},
+	}).Handshake()
+
+	if hello == nil {
+		return nil, err
+	}
+
+	return hello, nil
+}
+
+//handling tls connection
+
+func handleTlsConnection(clientConn net.Conn) {
+	defer clientConn.Close()
+
+	if err := clientConn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+		log.Print(err)
+		return
+	}
+
+	clientHello, clientReader, err := peekClientHello(clientConn)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	if err := clientConn.SetReadDeadline(time.Time{}); err != nil {
+		log.Print(err)
+		return
+	}
+
+	if !strings.HasSuffix(clientHello.ServerName, ".internal.example.com") {
+		log.Print("Blocking connection to unauthorized backend")
+		return
+	}
+
+	backendConn, err := net.DialTimeout("tcp", net.JoinHostPort(clientHello.ServerName, "443"), 5*time.Second)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer backendConn.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		io.Copy(clientConn, backendConn)
+		clientConn.(*net.TCPConn).CloseWrite()
+		wg.Done()
+	}()
+	go func() {
+		io.Copy(backendConn, clientReader)
+		backendConn.(*net.TCPConn).CloseWrite()
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
